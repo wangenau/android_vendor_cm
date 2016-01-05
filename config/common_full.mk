29a5da4f3f1a118d25ef09fa61f447810e471bfe
wangenau <wangenau@users.noreply.github.com>
2016-01-05 17:40:36 +0100

    Remove some apps

--- config/common_full.mk
+++ config/common_full.mk
@@ -10,22 +10,7 @@ include vendor/cm/config/cm_audio.mk
 # Include CM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 
-# Optional CM packages
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
-    PhotoTable \
-    SoundRecorder \
-    PhotoPhase
-
-PRODUCT_PACKAGES += \
-    VideoEditor \
     libvideoeditor_jni \
     libvideoeditor_core \
     libvideoeditor_osal \
