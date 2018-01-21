d7490c49da4fa11ad5ad71298c3f76a238e62f49
Wanja Schulze <rajaschu@googlemail.com>
2018-01-21 15:26:27 +0100

    Remove changelog
    It doesnt show up in settings anymore

--- config/common.mk
+++ config/common.mk
@@ -45,10 +45,6 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
-# Copy over the changelog to the device
-PRODUCT_COPY_FILES += \
-    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
-
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
