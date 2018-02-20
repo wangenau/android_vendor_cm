47d0439e2b3300814f307ba61f16ba85f9fbf38a
Wanja Schulze <rajaschu@googlemail.com>
2018-02-20 13:24:58 +0100

    Cleanup
    -ksm is dead
    -remove benchmark permissions
    -remove livewallpaper, it just a waste of battery,space and performance
     and google's new livewallpapers are not compatible

--- config/common_full.mk
+++ config/common_full.mk
@@ -7,10 +7,6 @@ include vendor/cm/config/cm_audio.mk
 # Include CM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 
- # Optional CM packages
- PRODUCT_PACKAGES += \
-    LiveWallpapersPicker
-
 # Extra tools in CM
 PRODUCT_PACKAGES += \
     vim \
