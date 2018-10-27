b6bafd4ce63f6fb3615596a0c4041c3529bdc0ce
Wanja Schulze <rajaschu@googlemail.com>
2018-10-27 15:31:20 +0200

    Update Fennec to 62.0.3

--- prebuilt/app/fennec/Android.mk
+++ prebuilt/app/fennec/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := Fennec
-LOCAL_SRC_FILES     := org.mozilla.fennec_fdroid_620200.apk
+LOCAL_SRC_FILES     := org.mozilla.fennec_fdroid_620300.apk
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := APPS
 LOCAL_CERTIFICATE   := PRESIGNED
